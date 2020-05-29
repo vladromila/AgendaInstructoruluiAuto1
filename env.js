95c17d10c02fbe97c92c375f19f94b35c27d5bf5
Vlad Romila <romilavlad2001@gmail.com>
2020-05-29 19:51:00 +0300

    Done

--- env.js
+++ env.js
@@ -1,8 +1,2 @@
 export const FIREBASE_CONFIG= {
-  apiKey: "AIzaSyC5pU1CA5mnbVKinw6WhlGdd-4WSQxJ2YM",
-  authDomain: "agendainstructoruluiauto.firebaseapp.com",
-  databaseURL: "https://agendainstructoruluiauto.firebaseio.com",
-  projectId: "agendainstructoruluiauto",
-  storageBucket: "agendainstructoruluiauto.appspot.com",
-  messagingSenderId: "57385892432"
   };
